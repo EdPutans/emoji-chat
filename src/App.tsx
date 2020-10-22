0f192fda3c5c41186a9cf46149bce7fb164fee68
Ed Putans <edputans@gmail.com>
2020-10-22 17:22:55 +0100

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 import SignInButton from './Components/SignInButton';
 
 fbase.initializeApp({
-  apiKey: 'AIzaSyDvg7xB31ooCOG1tQDVDKr25T3sHnAWIog',
-  authDomain: 'emoji-chat-6828e.firebaseapp.com',
-  databaseURL: 'https://emoji-chat-6828e.firebaseio.com',
-  projectId: 'emoji-chat-6828e',
-  storageBucket: 'emoji-chat-6828e.appspot.com',
-  messagingSenderId: '321217861790',
-  appId: '1:321217861790:web:b1e822ff09e8f7540b838f',
-  measurementId: 'G-6QB9M17JVF',
+
 });
 
 const Auth = fbase.auth();
