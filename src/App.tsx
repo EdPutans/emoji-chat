51b21a94d16287f10cb86c3f0fc94b275ab566ee
Ed Putans <edputans@gmail.com>
2020-10-25 12:45:11 +0000

    firebase returns data now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 
-import fbase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
@@ -9,28 +9,49 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import SignInButton from './Components/SignInButton';
+// import Chat from './Components/Chat';
 
-fbase.initializeApp({
-
+firebase.initializeApp({
+  // todo: create ENV for all data
 });
 
-const Auth = fbase.auth();
-const Fstore = fbase.firestore();
+const Auth = firebase.auth();
+const firestore = firebase.firestore();
+firestore.settings({ timestampsInSnapshots: true });
 
-const signIn = () => Auth.signInWithPopup(new fbase.auth.GoogleAuthProvider());
+const signIn = () => Auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
 const signOut = () => Auth.signOut();
 
-const App: React.FC = () => {
+const App = () => {
   const [user] = useAuthState(Auth);
 
   return (
     <div className="App">
       {user && <p> LOGGED IN!!! </p>}
       <SignInButton onClick={signIn} />
-      {user && <button onClick={signOut}>SIGN OUT?</button>}
+      <Chat />
+      {user && (
+      <>
+        <button onClick={signOut}>SIGN OUT?</button>
+      </>
+      )}
       <header />
     </div>
   );
 };
 
 export default App;
+
+const Chat = () => {
+  const dumdum = useRef();
+  const messagesRef = firestore.collection('messages').get().then((snap) => {
+    const res = snap.docs.map((doc) => doc.data());
+    console.log(res);
+  });
+  // const getMessagesQuery = messagesRef.orderBy('createdAt').limit(50);
+  // const [messages] = useCollectionData(getMessagesQuery, {
+  // snapshotListenOptions: { includeMetadataChanges: true },
+  // });
+  // console.log(messages);
+  return <div />;
+};
