3ee3c25b05f0df03f4d6f3ab7b453f871f46002b
Ed Putans <edputans@gmail.com>
2020-10-26 19:50:35 +0000

    attempt-firebase

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 const fireConfig = { // safe to display as per https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
-
   apiKey: 'AIzaSyDvg7xB31ooCOG1tQDVDKr25T3sHnAWIog',
   authDomain: 'emoji-chat-6828e.firebaseapp.com',
   databaseURL: 'https://emoji-chat-6828e.firebaseio.com',
